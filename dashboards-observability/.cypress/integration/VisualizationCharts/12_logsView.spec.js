2ee1f92f4ce3200a080f0458055f5dcbce869e88
Pratibha Pandey <pratibha_pandey@persistent.com>
2022-09-06 10:19:35 +0530

    Added test cases for Logs view
    Signed-off-by: Pratibha Pandey <pratibha_pandey@persistent.com>

--- dashboards-observability/.cypress/integration/VisualizationCharts/12_logsView.spec.js
+++ dashboards-observability/.cypress/integration/VisualizationCharts/12_logsView.spec.js
@@ -29,6 +29,8 @@ const renderLogsViewChart = () => {
     .type('{enter}');
 };
 
+const fieldName = 'host';
+
 describe('Render Logs view and verify default behavior', () => {
   beforeEach(() => {
     renderLogsView();
@@ -42,8 +44,10 @@ describe('Render Logs view and verify default behavior', () => {
     cy.get('.euiTitle.euiTitle--xxsmall').contains('Data Configurations').should('exist');
     cy.get('.euiTitle.euiTitle--xxsmall').contains('Columns');
     cy.get('.euiFormLabel.euiFormRow__label').contains('Field');
-    cy.get('.euiButton__text').contains('Add').should('be.disabled');
-    cy.get('.euiButton__text').contains('Update Chart').should('be.disabled');
+    cy.get('.euiButton.euiButton--primary.euiButton--fullWidth.euiButton-isDisabled').should(
+      'be.disabled'
+    );
+    cy.get('[data-test-subj="visualizeEditorRenderButton"]').should('be.disabled');
   });
 
   it('Render Logs view and verify Style section for Logs view', () => {
@@ -63,8 +67,19 @@ describe('Render Logs view and verify default behavior', () => {
     cy.get('.ag-header.ag-pivot-off').should('exist');
   });
 
-  it('Verify that Logs view details are appearing in tabular form', () => {
-    cy.get('[data-test-subj = "docTable"]').should('exist');
+  it('Verify Style section for Logs view', () => {
+    cy.get('#data-panel').contains('Style').should('exist');
+    cy.get('.euiAccordion__button').contains('Panel options').should('exist');
+    cy.get('#configPanel__panelOptions').contains('Title').should('exist');
+    cy.get('.euiFormHelpText.euiFormRow__text').contains('Name your visualization').should('exist');
+    cy.get('#configPanel__panelOptions').contains('Description').should('exist');
+  });
+
+  it('Add and Remove toggle buttons for fields section', () => {
+    cy.get('#available_fields').contains('Available Fields').should('exist');
+    cy.get('[aria-label="Add agent to table"]').should('be.disabled');
+    cy.get('#selected_fields').contains('Query fields').should('exist');
+    cy.get('[aria-label="Remove clientip from table"]').should('be.disabled');
   });
 });
 
@@ -79,11 +94,148 @@ describe('Save and Delete Visualization', () => {
   });
 });
 
-describe('', () => {
+describe('Render Logs view with no stats section in the query', () => {
+  beforeEach(() => {
+    renderLogsViewChart();
+  });
+
+  it('Disabled Table view toogle button', () => {
+    cy.get('[data-test-subj="workspace__dataTableViewSwitch"]').should('be.disabled');
+  });
+
+  it('Save toast message', () => {
+    const vis_name_sub_string = Math.floor(Math.random() * 100);
+    cy.get('[data-test-subj="eventExplorer__saveManagementPopover"]').click();
+    cy.get('[data-test-subj="eventExplorer__querySaveComboBox"]').click();
+    cy.get('.euiComboBoxOptionsList__rowWrap .euiFilterSelectItem').eq(0).click();
+    cy.get(
+      '.euiPopover__panel .euiFormControlLayoutIcons [data-test-subj="comboBoxToggleListButton"]'
+    )
+      .eq(0)
+      .click();
+    cy.get('.euiPopover__panel input')
+      .eq(1)
+      .type(`Test visualization` + vis_name_sub_string);
+    cy.get('[data-test-subj="eventExplorer__querySaveConfirm"]').click();
+    cy.get('[data-test-subj="euiToastHeader"]')
+      .contains('There is no query or(and) visualization to save')
+      .should('exist');
+  });
+
+  it('Verify Logs view details when PPL query does not have stats section ', () => {
+    cy.get('[data-test-subj="docTable"]').should('exist');
+    cy.get('.osdDocTableHeader').contains('Time').should('exist');
+    cy.get('.osdDocTableHeader').contains('_source').should('exist');
+  });
+
+  it('Add and Remove toggle buttons for fields section should be enabled', () => {
+    //Add field
+    cy.get('[data-test-subj="fieldToggle-agent"]').click();
+    cy.get('[data-test-subj="field-agent"]').should('exist');
+    //Remove field
+    cy.get('[data-test-subj="fieldToggle-agent"]').click();
+    cy.get('[aria-labelledby="selected_fields"] [data-test-subj="field-agent-showDetails"]').should(
+      'not.exist'
+    );
+  });
+
+  it('Search engine for fields under Visualizations', () => {
+    cy.get('[data-test-subj="eventExplorer__sidebarSearch"]').should('exist').type(fieldName);
+    cy.get('[data-test-subj="fieldToggle-host"]').click();
+    cy.get('[data-test-subj="fieldList-selected"]').should('exist');
+  });
+
+  it('View surrounding events button enabled', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('.euiButton__text').eq(4).should('not.be.disabled');
+  });
+});
+
+describe('Event Details overlay', () => {
   beforeEach(() => {
     renderLogsView();
   });
 
+  it('Verify Event Details overaly should get opened after clicking on details toggle button', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+  });
 
+  it('Options in Event Details overlay', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('.euiTabs .euiTab__content').contains('Table');
+    cy.get('.table.table-condensed.osdDocViewerTable').should('exist');
+    cy.get('.euiTabs .euiTab__content').contains('JSON').click();
+    cy.get('.euiCodeBlock__code.json').should('exist');
+    cy.get('.euiTabs .euiTab__content').contains('Traces').click();
+    cy.get('.euiCallOutHeader__title').contains('No Trace Id found in the event.').should('exist');
+    cy.get('.euiLink.euiLink--primary').contains('Trace Analytics').click();
+    cy.get('#trace-analytics').contains('Trace Analytics').should('exist');
+    cy.get('.euiLink.euiLink--primary').contains('Log Correlation').click();
+    cy.get('#log-correlation').contains('Log Correlation').should('exist');
+  });
 
+  it('View surrounding events button disabled', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('.euiButton.euiButton--primary.euiButton-isDisabled.header-button')
+      .contains('View surrounding events')
+      .should('be.disabled');
+  });
+
+  it('Event Details overlay resizable and "X" buttons', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('[title="Resize"]').click();
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('[data-test-subj="euiFlyoutCloseButton"]').should('exist').click();
+  });
+
+  it('Table details on Event Details overlay', () => {
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--inherit.euiButtonIcon__icon')
+      .eq(2)
+      .click({ force: true });
+    cy.get('#eventsDocFyout').contains('Event Details').should('exist');
+    cy.get('[data-test-subj="docTable"]').should('exist');
+  });
+});
+
+describe('Data Configuration panel when no stats in the query', () => {
+  beforeEach(() => {
+    renderLogsViewChart();
+  });
+
+  it('Data Configuration panel default behavior', () => {
+    cy.get('.euiTitle.euiTitle--xxsmall').contains('Data Configurations').should('exist');
+    cy.get('.euiTitle.euiTitle--xxsmall').contains('Columns').should('exist');
+    cy.get('.euiButton__text').eq(2).contains('Add').should('not.be.disabled');
+    cy.get('.euiButton__text').eq(3).contains('Update Chart').should('not.be.disabled');
+  });
+
+  it('Add button in Data Configuration panel', () => {
+    cy.get('.euiTitle.euiTitle--xxsmall').contains('Data Configurations').should('exist');
+    cy.get('.euiTitle.euiTitle--xxsmall').contains('Columns').should('exist');
+    cy.get('.euiButton__text').eq(2).contains('Add').should('not.be.disabled').click();
+    cy.get('.euiFormLabel.euiFormRow__label').contains('Field');
+    cy.get('.euiIcon.euiIcon--medium.euiIcon--danger').should('exist');
+    cy.get('[data-test-subj="comboBoxToggleListButton"]').eq(0).click();
+    cy.get('.euiFlexItem.euiFilterSelectItem__content').eq(1).click();
+    cy.get('.euiButton__text').contains('Update Chart').should('not.be.disabled').click();
+    cy.get('[data-test-subj="docTable"]').should('exist');
+  });
 });
